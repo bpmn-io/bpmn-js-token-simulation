22022ff5cd82dbf0e3ed601271115a24fdc87a31
Nico Rehwaldt <git_nikku@nixis.de>
2018-12-02 21:17:01 +0100

    chore(project): add test + coverage infrastructure

---
+++ karma.conf.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var coverage = process.env.COVERAGE;
+
+var path = require('path');
+
+var absoluteBasePath = path.resolve(__dirname);
+
+/* global process */
+
+// configures browsers to run test against
+// any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE', 'PhantomJS' ]
+var browsers =
+  (process.env.TEST_BROWSERS || 'ChromeHeadless')
+    .replace(/^\s+|\s+$/, '')
+    .split(/\s*,\s*/g)
+    .map(function(browser) {
+      if (browser === 'ChromeHeadless') {
+        process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+        // workaround https://github.com/GoogleChrome/puppeteer/issues/290
+        if (process.platform === 'linux') {
+          return 'ChromeHeadless_Linux';
+        }
+      }
+
+      return browser;
+    });
+
+var suite = coverage ? 'test/all.js' : 'test/suite.js';
+
+
+module.exports = function(karma) {
+  karma.set({
+
+    frameworks: [
+      'mocha',
+      'sinon-chai'
+    ],
+
+    files: [
+      { pattern: 'assets/fonts/*', included: false, served: true },
+      suite
+    ],
+
+    preprocessors: {
+      [suite]: [ 'webpack' ]
+    },
+
+    reporters: [ 'spec' ].concat(coverage ? 'coverage' : []),
+
+    browsers: browsers,
+
+    browserNoActivityTimeout: 30000,
+
+    customLaunchers: {
+      ChromeHeadless_Linux: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ],
+        debug: true
+      }
+    },
+
+    coverageReporter: {
+      reporters: [
+        { type: 'lcov', subdir: '.' }
+      ]
+    },
+
+    autoWatch: false,
+    singleRun: true,
+
+    webpack: {
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            test: /\.(css|bpmn)$/,
+            use: 'raw-loader'
+          },
+          {
+            test: /\.png$/,
+            use: 'url-loader'
+          }
+        ].concat(
+          coverage ? {
+            test: /\.js$/,
+            use: {
+              loader: 'istanbul-instrumenter-loader',
+              options: { esModules: true }
+            },
+            include: /lib\.*/,
+            exclude: /node_modules/
+          } : []
+        )
+      },
+      resolve: {
+        mainFields: [
+          'dev:module',
+          'browser',
+          'module',
+          'main'
+        ],
+        modules: [
+          'node_modules',
+          absoluteBasePath
+        ]
+      }
+    }
+  });
+};
