5f993c1c865e6a2f8da6a972558a8257c5f94e18
Philipp Fromme <philippfromme@outlook.com>
2018-12-03 17:10:24 +0100

    test(token-simulation): add basic test scenarios
    Closes #37

---
+++ test/mocks/Animation.js
@@ -0,0 +1,19 @@
+function noop() {}
+
+function Animation() {
+  this.animations = [];
+  this.hiddenAnimations = [];
+
+  this.createAnimation = function(connection, processInstanceId, done) {
+    setTimeout(done);
+  };
+
+  this.setAnimationSpeed = noop;
+  this.showProcessInstanceAnimations = noop;
+  this.hideProcessInstanceAnimations = noop;
+}
+
+export default {
+  __init__: [ 'animation' ],
+  animation: [ 'type', Animation]
+};
