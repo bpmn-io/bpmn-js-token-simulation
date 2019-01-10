7fa6d94397c8f3d4472ee4b6040d9a99775d7fc9
Nico Rehwaldt <git_nikku@nixis.de>
2019-01-10 15:03:07 +0100

    test(simulation): add viewer spec

---
+++ test/spec/ViewerSpec.js
@@ -0,0 +1,30 @@
+import TokenSimulationViewerModules from 'lib/viewer';
+import NavigatedViewer from 'bpmn-js/lib/NavigatedViewer';
+
+import {
+  bootstrapViewer,
+  inject
+} from 'test/TestHelper';
+
+
+describe('token-simulation - viewer', function() {
+
+  const diagram = require('example/resources/example.bpmn');
+
+  beforeEach(bootstrapViewer(diagram, {
+    additionalModules: [].concat(NavigatedViewer.prototype._modules).concat([
+      TokenSimulationViewerModules
+    ]),
+    keyboard: {
+      bindTo: document
+    }
+  }));
+
+
+  it.only('should toggle mode', inject(function(toggleMode) {
+
+    // YEA!
+    toggleMode.toggleMode();
+  }));
+
+});
