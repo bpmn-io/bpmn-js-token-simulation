a5f18dce65db2378f5c06ee44c6bfdce1a3c3868
Nico Rehwaldt <git_nikku@nixis.de>
2021-05-13 10:58:09 +0200

    feat(example): persist toggle state

--- example/src/modeler.js
+++ example/src/modeler.js
@@ -6,68 +6,86 @@ import fileDrop from 'file-drops';
 
 import exampleXML from '../resources/example.bpmn';
 
+const url = new URL(window.location.href);
 
-const persistent = new URL(window.location.href).searchParams.has('p');
+const persistent = url.searchParams.has('p');
+const active = url.searchParams.has('e');
 
-function diagram() {
+const initialDiagram = (() => {
   try {
     return persistent && localStorage['diagram-xml'] || exampleXML;
   } catch (err) {
     return exampleXML;
   }
-}
+})();
 
-const persistModule = persistent ? {
+
+const ExampleModule = {
   __init__: [
-    function(eventBus, bpmnjs) {
-      eventBus.on('commandStack.changed', function() {
-        bpmnjs.saveXML().then(result => {
-          localStorage['diagram-xml'] = result.xml;
+    [ 'eventBus', 'bpmnjs', 'toggleMode', function(eventBus, bpmnjs, toggleMode) {
+
+      if (persistent) {
+        eventBus.on('commandStack.changed', function() {
+          bpmnjs.saveXML().then(result => {
+            localStorage['diagram-xml'] = result.xml;
+          });
         });
+      }
+
+      if ('history' in window) {
+        eventBus.on('tokenSimulation.toggleMode', event => {
+
+          if (event.active) {
+            url.searchParams.set('e', '1');
+          } else {
+            url.searchParams.delete('e');
+          }
+
+          history.replaceState({}, document.title, url.toString());
+        });
+      }
+
+      eventBus.on('diagram.init', 500, () => {
+        toggleMode.toggleMode(active);
       });
-    }
+    } ]
   ]
-} : {};
+};
 
 const modeler = new BpmnModeler({
   container: '#canvas',
   additionalModules: [
     TokenSimulationModule,
-    persistModule
+    ExampleModule
   ],
   keyboard: {
     bindTo: document
   }
 });
 
-modeler.importXML(diagram())
-  .then(({ warnings }) => {
-    if (warnings.length) {
-      console.warn(warnings);
-    }
-
-    modeler.get('canvas').zoom('fit-viewport');
-  })
-  .catch(err => {
-    console.error(err);
-  });
-
-document.body.addEventListener('dragover', fileDrop('Open BPMN diagram', function(files) {
-
-  // files = [ { name, contents }, ... ]
-
-  const file = files[0];
-
-  file && modeler.importXML(file.contents)
+modeler.openDiagram = function(diagram) {
+  return this.importXML(diagram)
     .then(({ warnings }) => {
       if (warnings.length) {
         console.warn(warnings);
       }
 
-      modeler.get('canvas').zoom('fit-viewport');
+      this.get('canvas').zoom('fit-viewport');
     })
     .catch(err => {
       console.error(err);
     });
+};
+
+document.body.addEventListener('dragover', fileDrop('Open BPMN diagram', function(files) {
+
+  // files = [ { name, contents }, ... ]
+
+  if (files.length) {
+    modeler.openDiagram(files[0].contents);
+  }
 
 }), false);
+
+
+modeler.openDiagram(initialDiagram);
--- example/src/viewer.js
+++ example/src/viewer.js
@@ -7,9 +7,56 @@ import fileDrop from 'file-drops';
 import exampleXML from '../resources/example.bpmn';
 
 
+const url = new URL(window.location.href);
+
+const persistent = url.searchParams.has('p');
+const active = url.searchParams.has('e');
+
+const initialDiagram = (() => {
+  try {
+    return persistent && localStorage['diagram-xml'] || exampleXML;
+  } catch (err) {
+    return exampleXML;
+  }
+})();
+
+
+const ExampleModule = {
+  __init__: [
+    [ 'eventBus', 'bpmnjs', 'toggleMode', function(eventBus, bpmnjs, toggleMode) {
+
+      if (persistent) {
+        eventBus.on('commandStack.changed', function() {
+          bpmnjs.saveXML().then(result => {
+            localStorage['diagram-xml'] = result.xml;
+          });
+        });
+      }
+
+      if ('history' in window) {
+        eventBus.on('tokenSimulation.toggleMode', event => {
+
+          if (event.active) {
+            url.searchParams.set('e', '1');
+          } else {
+            url.searchParams.delete('e');
+          }
+
+          history.replaceState({}, document.title, url.toString());
+        });
+      }
+
+      eventBus.on('diagram.init', 500, () => {
+        toggleMode.toggleMode(active);
+      });
+    } ]
+  ]
+};
+
 const viewer = new BpmnViewer({
   container: '#canvas',
   additionalModules: [
+    ExampleModule,
     TokenSimulationModule
   ],
   keyboard: {
@@ -17,35 +64,28 @@ const viewer = new BpmnViewer({
   }
 });
 
-viewer.importXML(exampleXML)
-  .then(({ warnings }) => {
-    if (warnings.length) {
-      console.warn(warnings);
-    }
-
-    viewer.get('canvas').zoom('fit-viewport');
-  })
-  .catch(err => {
-    console.error(err);
-  });
-
-
-document.body.addEventListener('dragover', fileDrop('Open BPMN diagram', function(files) {
-
-  // files = [ { name, contents }, ... ]
-
-  const file = files[0];
-
-  file && viewer.importXML(file.contents)
+viewer.openDiagram = function(diagram) {
+  return this.importXML(diagram)
     .then(({ warnings }) => {
       if (warnings.length) {
         console.warn(warnings);
       }
 
-      viewer.get('canvas').zoom('fit-viewport');
+      this.get('canvas').zoom('fit-viewport');
     })
     .catch(err => {
       console.error(err);
     });
+};
+
+document.body.addEventListener('dragover', fileDrop('Open BPMN diagram', function(files) {
+
+  // files = [ { name, contents }, ... ]
+
+  if (files.length) {
+    viewer.openDiagram(files[0].contents);
+  }
 
 }), false);
+
+viewer.openDiagram(initialDiagram);
