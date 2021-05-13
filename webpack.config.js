90072ee370d9db304fcdcf37639e23dc1d5fde57
Nico Rehwaldt <git_nikku@nixis.de>
2021-05-13 10:53:27 +0200

    chore(example): generate source maps in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,42 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    viewer: './example/src/viewer.js',
-    modeler: './example/src/modeler.js'
-  },
-  output: {
-    filename: 'dist/[name].js',
-    path: __dirname + '/example'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.bpmn$/,
-        use: {
-          loader: 'raw-loader'
+module.exports = (env, argv) => {
+
+  const mode = argv.mode || 'development';
+
+  const config = {
+    mode,
+    entry: {
+      viewer: './example/src/viewer.js',
+      modeler: './example/src/modeler.js'
+    },
+    output: {
+      filename: 'dist/[name].js',
+      path: __dirname + '/example'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.bpmn$/,
+          use: {
+            loader: 'raw-loader'
+          }
         }
-      }
-    ]
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: './node_modules/bpmn-js/dist/assets', to: 'dist/vendor/bpmn-js/assets' },
-        { from: './assets', to: 'dist/vendor/bpmn-js-token-simulation/assets' }
       ]
-    })
-  ]
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './node_modules/bpmn-js/dist/assets', to: 'dist/vendor/bpmn-js/assets' },
+          { from: './assets', to: 'dist/vendor/bpmn-js-token-simulation/assets' }
+        ]
+      })
+    ]
+  };
+
+  if (mode === 'production') {
+    config.devtool = 'source-map';
+  }
+
+  return config;
 };
