f3e70b70bd05b2bd084981675fcdce7b8fdd1d80
Nico Rehwaldt <git_nikku@nixis.de>
2021-11-30 09:22:28 +0100

    test: increase `all` test timeout
    Simulating a huge diagram takes time.

--- test/spec/SimulationSpec.js
+++ test/spec/SimulationSpec.js
@@ -491,7 +491,7 @@ describe('simulation', function() {
     }));
 
 
-    this.timeout(5000);
+    this.timeout(10000);
 
     it('should simulate diagram', inject(
       async function(simulator) {
