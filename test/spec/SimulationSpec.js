53d026e818c7538f141700e77778bdf19c5abe4f
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-11-07 14:47:01 +0100

    test: change animation speed to reliably trigger <all> test

--- test/spec/SimulationSpec.js
+++ test/spec/SimulationSpec.js
@@ -1217,7 +1217,7 @@ describe('simulation', function() {
       async function(simulator, animation) {
 
         // given
-        animation.setAnimationSpeed(1000);
+        animation.setAnimationSpeed(500);
 
         // when
         triggerElement('ALL_START');
