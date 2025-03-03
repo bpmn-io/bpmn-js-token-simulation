15b3b058bbbce49457f9d785bda2635f2d2bc299
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-03-03 21:19:41 +0100

    WIP

--- test/spec/simulator/SimulatorSpec.js
+++ test/spec/simulator/SimulatorSpec.js
@@ -1841,6 +1841,18 @@ describe('simulator', function() {
       expectTrace(fixture());
     });
 
+
+    verify('compensation-compensate-scope', (fixture) => {
+
+      // when
+      trigger({
+        element: element('START')
+      });
+
+      // then
+      expectTrace(fixture());
+    });
+
   });
 
 
